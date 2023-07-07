6e89171f00f633c755f84a5c41b48e3c5cc4b9e1
Gdel-J <jessygdelvecchio@gmail.com>
2023-07-07 11:21:57 -0400

    webpack.config.js file implemented and comment added

--- client/src-sw.js
+++ client/src-sw.js
@@ -7,7 +7,7 @@ const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
 
 precacheAndRoute(self.__WB_MANIFEST);
 
-const pageCache = new CacheFirst({
+const pageCache = new CacheFirst({  // The CacheFirst strategy will fetch resources from the cache first and only go to the network if the resource is not already in the cache.
   cacheName: 'page-cache',
   plugins: [
     new CacheableResponsePlugin({
@@ -19,16 +19,22 @@ const pageCache = new CacheFirst({
   ],
 });
 
-warmStrategyCache({
+
+
+warmStrategyCache({  // The warmStrategyCache method preloads certain URLs with a specific caching strategy, here, the 'page-cache' set up above.
   urls: ['/index.html', '/'],
   strategy: pageCache,
 });
 
-registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+registerRoute(({ request }) => request.mode === 'navigate', pageCache); //when a navigation request is made (a page load, not fetches made from the page), it will use the 'page-cache' caching strategy.
+
+
 
 //  Implement asset caching
+
+
 registerRoute(
-({request}) => ["style","script","worker"].includes(request.destination),
+({request}) => ["style","script","worker"].includes(request.destination),//These lines of code  set up caching for specific types of resources - stylesheets, scripts, and web workers. They use the 'Stale While Revalidate' strategy, which means that it will return cached data if available, otherwise it will fetch from the network.
 
 new StaleWhileRevalidate({
   cacheName: "asset-cache",
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,29 +1,85 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+const { InjectManifest } = require("workbox-webpack-plugin");
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+//  Added configured workbox plugins for a service worker and manifest file.
+const WorkboxPlugin = require("workbox-webpack-plugin");
+
+// added CSS loaders and babel to webpack.
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: "development", // Specifies the mode to use.
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      //An object defining the entry points for the application. These are the top-level files that Webpack will include in the bundle.
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
     },
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: "[name].bundle.js", //name of the output
+      path: path.resolve(__dirname, "dist"), //output directory
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        //It simplifies the creation of HTML files to serve our webpack bundles.
+        template: "./index.html",
+        title: "Webpack Plugin",
+      }),
+      new MiniCssExtractPlugin(), //This plugin extracts CSS into separate files. It creates a CSS file per JS file which contains CSS.
+
+      new InjectManifest({
+        //This plugin injects a list of URLs into a service worker file, generating a list of local assets that should be cached.
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
+      }),
+
+      new WebpackPwaManifest({
+        //This plugin generates a 'web app manifest' for Progressive Web Apps (PWA), with auto icon resizing and fingerprinting support.
+        fingerprints: false,
+        inject: true,
+        name: "Just Another Text Editor",
+        short_name: "J.A.T.E",
+        description: "offline text editor",
+        background_color: "#2565b3",
+        theme_color: "#2565b3",
+        start_url: "/",
+        publicPath: "/",
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512], //multiple sizes
+            destination: path.join("assets", "icons"),
+          },
+        ],
+      }),
     ],
 
-    module: {
+    module: {//: It tells Webpack how to handle different types of modules. The rules array consists of loader objects, which define a test (what kind of files to process) and a use (what loader or loaders to use).
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.(png|jpg|svg|jpeg|gif)$/i,
+          type: "asset/ressource",
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
+            },
+          },
+        },
       ],
     },
   };
